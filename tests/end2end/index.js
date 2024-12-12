37b745816573cf69d50153d370b144367d0baeeb
tirtahakimpambudhi <awebthp@gmail.com>
2024-12-12 16:50:25 +0700

    test: :white_check_mark: Update unit tests in the file index.js

--- tests/end2end/index.js
+++ tests/end2end/index.js
@@ -22,7 +22,7 @@ newman.run({
 },function (err) {
     if (err) {
         console.error(err?.message);
-        process.exit(1);
+        throw err;
         return;
     }
     console.log('collection run complete!');
